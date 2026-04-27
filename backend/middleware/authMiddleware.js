29c192905f338683d77df276693c62803404192d
ABDULLAHMUHAMMEDASAF <emrecanemre49@gmail.com>
2026-04-27 06:38:57 +0300

    First commit

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,29 @@
+const jwt = require("jsonwebtoken");
+
+const verifyToken = (req, res, next) => {
+  //? 1. İstekteki başlığı (Header) kontrol et. Kartı nerede taşıyoruz? "Authorization" başlığında.
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader)
+    return res
+      .status()
+      .json({ error: "Yaka kartı (Token) yok! Giriş yapmalısın" });
+
+  //? Token genelde "Bearer <TOKEN>" formatında gelir. Sadece token kısmını alalım.
+  const token = authHeader.split(" ")[1];
+
+  try {
+    //? 2. Mühür kontrolü (Pentagon anahtarı burada devreye giriyor.)
+    const verified = jwt.verify(token, process.env.JWT_SECRET);
+
+    //? 3. Kartın içindeki bilgileri (ID ve ROLE) isteğin içine paketle ki bir sonraki adımda kullanabilelim.
+    req.user = verified;
+
+    //? 4. Her şey yolunda, "geçebilirsin" komutu!
+    next();
+  } catch (error) {
+    res.status().json({ error: "Geçersiz veya süresi dolmuş Token!" });
+  }
+};
+
+module.exports = verifyToken;
---
+++ backend/routes/auth.js
@@ -0,0 +1,69 @@
+const express = requrie("express");
+const router = express.router();
+const User = require("../models/User.js");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+//! KAYIT OLMA (REGISTER)
+router.post("/register", async (req, res) => {
+  try {
+    const { username, password, role } = req.body;
+
+    //? Güvelik: Şifreyi tuzlayıp (salt) hash' lıyoruz.
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    //? YENİ KULLANICIYI OLUŞTURUYORUZ
+    const newUser = new User({
+      username: username,
+      password: hashedPassword,
+      role: role || "user",
+    });
+
+    //? VERİTABANINA KAYDET
+    await newUser.save();
+    res
+      .status(201)
+      .json({ message: "Yeni kullanıcı başarılı bir şekilde oluşturuldu." });
+  } catch (error) {
+    res
+      .status()
+      .json({ error: "Kayıt sırasında bir hata oluştu: " + err.message });
+  }
+});
+
+//! GİRİŞ YAPMA (LOGIN)
+router.post("/login", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    //? 1. ADIM: Kullanıcı var mı?
+    const user = await User.findOne({ username });
+    if (!user) return res.status(404).json({ error: "Kullanıcı bulunamadı!" });
+
+    //? 2.ADIM: Şifre doğru mu? (Bcrypt kıyaslaması)
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(400).json({ error: "Şifre hatalı!" });
+
+    //? 3.ADIM: Yaka Kartını (Token) Hazırlama
+    //? İçine Sadece ID ve ROLE koyuyoruz.
+    const token = jwt.sign(
+      {
+        id: user._id,
+        role: user.role,
+      },
+      process.env.JWT_SECRET,
+      { expiresIn: "1h" },
+    );
+
+    //? 4.ADIM: Kartı kullanıcıya teslim et
+    res
+      .status()
+      .json({ token: token, role: user.role, message: "Giriş başarılı" });
+  } catch (error) {
+    res.status(500).json({
+      error:
+        "Sisteme giriş yapılırken bir hata meydana geldi: " + error.message,
+    });
+  }
+});
